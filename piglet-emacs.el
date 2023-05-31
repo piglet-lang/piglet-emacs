7d09f4542fe741039d7c8f8ec3ee6581cb0617c5
Arne Brasseur <arne@arnebrasseur.net>
2023-05-31 12:24:36 +0200

    Add piglet-emacs entry point

---
+++ piglet-emacs.el
@@ -0,0 +1,19 @@
+;;; piglet-emacs.el --- -*- lexical-binding: t -*-
+
+;; Author: Arne Brasseur <arne@arnebrasseur.net>
+;; Filename: piglet-emacs.el
+
+;;; Commentary:
+
+;; Mainly here as an entry point so a simple (use-package 'piglet-emacs) loads
+;; the full package.
+
+;;; Code:
+
+(require 'piglet-mode)
+(require 'pdp)
+
+(provide 'piglet-emacs)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; piglet-emacs.el ends here
